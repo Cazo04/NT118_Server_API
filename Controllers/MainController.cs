e22e99500597151e040dd846f6adff9ee5e47c09
Cazo <Cazo@DESKTOP-3TO1BJC>
2023-09-25 14:25:36 +0700

    Thêm MainController

---
+++ Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NT118_Server_API.Models;
+
+namespace NT118_Server_API.Controllers
+{
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/NhanVien")]
+        public async Task<IActionResult> TestGet()
+        {
+            return Ok(new NhanVien()
+            {
+
+            });
+        }
+    }
+}
