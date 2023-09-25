ee8ff2b84cee9ff3c47e86ee127dd27c5508e5b5
Cazo <Cazo@DESKTOP-3TO1BJC>
2023-09-25 13:13:38 +0700

    Thêm Models

---
+++ Models/NhanVien.cs
@@ -0,0 +1,18 @@
+﻿namespace NT118_Server_API.Models
+{
+    public class NhanVien
+    {
+        public string MANV { get; set; }
+        public string MK { get; set; }
+        public string HOTEN { get; set; }
+        public string GIOITINH { get; set; }
+        public DateTime NGSINH { get; set; }
+        public DateTime NGVL { get; set; }
+        public string DC { get; set; }
+        public string SDT { get; set; }
+        public string EMAIL { get; set; }
+        public string CCCD { get; set; }
+        public decimal LCB { get; set; }
+        public string PHBAN { get; set; }
+    }
+}
---
+++ Models/TinNhan.cs
@@ -0,0 +1,12 @@
+﻿namespace NT118_Server_API.Models
+{
+    public class TinNhan
+    {
+        public string NGGUI { get; set; }
+        public string NGNHAN { get; set; }
+        public DateTime TG { get; set; }
+        public string LTN { get; set; }
+        public string ND { get; set; }
+        public string TT { get; set; }
+    }
+}
